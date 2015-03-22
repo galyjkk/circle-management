1213098f118e948875dad2398e5eca9eb8385bcc
galyjkk <galyjkk@126.com>
2015-03-22 16:17:40 +0800

    circle management

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name circleManagementApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the circleManagementApp
+ */
+angular.module('circleManagementApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name circleManagementApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the circleManagementApp
+ */
+angular.module('circleManagementApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
